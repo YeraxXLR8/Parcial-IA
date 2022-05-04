78ded60388385b0b0b1b856ae04de034e59e434c
YeraxXLR8 <roberto109926@gmail.com>
2022-05-04 02:06:51 -0300

    boids completados

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -9,6 +9,11 @@ public class Boid : MonoBehaviour
     public float maxForce;
 
     public float viewRadius;
+
+    public float separationWeight;
+    public float alignmentWeight;
+    public float cohesionWeight;
+
     
     // Start is called before the first frame update
     void Start()
@@ -27,15 +32,13 @@ public class Boid : MonoBehaviour
         transform.position += _velocity * Time.deltaTime;
         transform.forward = _velocity;
 
-        Separation();
         CheckBounds();
-    }
-    void ApplyForce(Vector3 force)
-    {
-        _velocity += force;
+        ApplyForce(Separation() * separationWeight);
+        ApplyForce(Alignment() * alignmentWeight);
+        ApplyForce(Cohesion() * cohesionWeight);
     }
 
-    void Separation()
+    Vector3 Separation()
     {
         Vector3 desired = new Vector3();
 
@@ -46,18 +49,19 @@ public class Boid : MonoBehaviour
            Vector3 distance = boid.transform.position - transform.position;
             if (distance.magnitude < viewRadius)
             {
-                desired += distance; 
+                desired.x += distance.x;
+                desired.z += distance.z;
             }
         }
 
-        if (desired == Vector3.zero) return;
+        if (desired == Vector3.zero) return desired;
         desired *= -1;
         desired = desired.normalized * maxSpeed;
 
         Vector3 steering = desired - _velocity;
         steering = Vector3.ClampMagnitude(steering, maxForce / 10);
 
-        ApplyForce(steering);
+        return (steering);
     }
 
     void CheckBounds()
@@ -67,9 +71,72 @@ public class Boid : MonoBehaviour
         if (transform.position.z >= 20 || transform.position.z <= -20)
             transform.position = new Vector3(transform.position.x, 0, transform.position.z * -0.95f);
     }
+
+    Vector3 Alignment ()
+    {
+        Vector3 desired = new Vector3();
+        int count = 0;
+
+        foreach (var item in BoidManager.instance.allBoids)
+        {
+            if (item == this) continue;
+
+            if (Vector3.Distance(item.transform.position, transform.position) < viewRadius)
+            {
+                desired += item._velocity;
+                desired.y = 0;
+                count++;
+            }
+
+        }
+
+        if (count == 0) return desired;
+
+        desired /= count;
+
+        return CalculateSteering(desired);
+    }
+
+    Vector3 Cohesion()
+    {
+        Vector3 desired = new Vector3();
+        int count = 0;
+
+        foreach (var item in BoidManager.instance.allBoids)
+        {
+            if (item == this) continue;
+
+            if (Vector3.Distance(item.transform.position,transform.position) < viewRadius)
+            {
+                desired += item.transform.position;
+                desired.y = 0;
+                count++;
+            }
+        }
+
+        if (count == 0) return desired;
+        desired /= count;
+        desired = desired - transform.position;
+
+        return (CalculateSteering(desired));
+    }
+    void ApplyForce(Vector3 force)
+    {
+        _velocity = Vector3.ClampMagnitude(_velocity + force, maxSpeed);
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position,viewRadius);
+    }
+
+    Vector3 CalculateSteering(Vector3 desired)
+    {
+        desired = desired.normalized * maxSpeed;
+
+        Vector3 steering = desired - _velocity;
+        steering = Vector3.ClampMagnitude(steering, maxForce / 10);
+
+        return steering;
     }
 }
